b86b2611b11016385a96c1c66bf239582bde64cd
Frans Stofberg <frans.stofberg@momentum.co.za>
2019-01-10 06:51:32 +0200

    added autocomplete dotfile

--- setup.sh
+++ setup.sh
@@ -4,11 +4,15 @@ rm $HOME/.vimrc
 rm $HOME/.zshrc
 rm $HOME/.mainrc
 rm $HOME/.oh-my-zsh
+rm $HOME/.autocomplete
 
 ln -s $HOME/terminal-care-package/dotfiles/vimrc $HOME/.vimrc
 ln -s $HOME/terminal-care-package/dotfiles/zshrc $HOME/.zshrc
+ln -s $HOME/terminal-care-package/dotfiles/autocomplete $HOME/.autocomplete
 ln -s $HOME/terminal-care-package/rcfiles/mainrc $HOME/.mainrc
 ln -s $HOME/terminal-care-package/oh-my-zsh/ $HOME/.oh-my-zsh
+
+
 
 echo "Symbolic links created"
 
