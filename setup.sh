a41474bf3930abb81f77befa8b94c364f44c805c
Ilzanne Stofberg <ilzanne.stofberg@gmail.com>
2020-02-05 22:23:32 +0200

    add vundle and clean up

--- setup.sh
+++ setup.sh
@@ -3,14 +3,21 @@
 rm $HOME/.vimrc
 rm $HOME/.zshrc
 rm $HOME/.mainrc
-rm $HOME/.oh-my-zsh
 rm $HOME/.autocomplete
+rm $HOME/.oh-my-zsh
+rm $HOME/.vim/bundle
+
+mkdir -p $HOME/.vim/bundle
 
 ln -s $HOME/terminal-care-package/dotfiles/vimrc $HOME/.vimrc
 ln -s $HOME/terminal-care-package/dotfiles/zshrc $HOME/.zshrc
-ln -s $HOME/terminal-care-package/dotfiles/autocomplete $HOME/.autocomplete
 ln -s $HOME/terminal-care-package/rcfiles/mainrc $HOME/.mainrc
+ln -s $HOME/terminal-care-package/dotfiles/autocomplete $HOME/.autocomplete
 ln -s $HOME/terminal-care-package/oh-my-zsh/ $HOME/.oh-my-zsh
+ln -s $HOME/terminal-care-package/vim/Vundle.vim $HOME/.vim/bundle/Vundle.vim
+
+# Install vim plugins
+vim +PluginInstall +qall
 
 # Add oh-my-zsh theme
 rm $HOME/.oh-my-zsh/themes/agnoster-fstof.zsh-theme
@@ -48,3 +55,4 @@ fi
 
 echo "Powerline font installed to $font_dir"
 
+vim $HOME/.mainrc
