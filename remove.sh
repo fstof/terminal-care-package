5511d72a927c37fb8b56397833512a6ce98536f6
Frans Stofberg <frans.stofberg@momentum.co.za>
2026-09-15 12:48:10 +0200

    remove neovim config on cleanup

--- remove.sh
+++ remove.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-
 echo "removing previously linked dotfiles"
 
 rm -rf $HOME/.oh-my-zsh
@@ -11,5 +10,6 @@ rm -rf $HOME/.mainrc
 rm -rf $HOME/.autocomplete
 rm -rf $HOME/.vim/bundle
 rm -rf $HOME/.p10k.zsh
+rm -rf $HOME/.config/nvim
 
 echo "Symbolic links deleted"
