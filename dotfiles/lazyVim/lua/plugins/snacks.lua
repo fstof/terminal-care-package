b697c77627d2a2f111e45c50b6e1d9b2faba2d17
Frans Stofberg <frans.stofberg@momentum.co.za>
2026-09-17 00:28:35 +0200

    hide explorer input box

--- dotfiles/lazyVim/lua/plugins/snacks.lua
+++ dotfiles/lazyVim/lua/plugins/snacks.lua
@@ -5,6 +5,9 @@ return {
       picker = {
         sources = {
           explorer = {
+            layout = {
+              auto_hide = { "input" }, -- Instantly hides the search box when focus moves to the file list
+            },
             hidden = true, -- Always show hidden/dotfiles in the tree explorer
             ignored = true, -- Optional: set to true to also see .gitignore'd files
           },
